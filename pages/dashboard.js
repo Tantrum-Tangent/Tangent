9ea188bcee42915b5b33141f894b556ca9480a54
Shezad <shezadhassan22@gmail.com>
2022-05-20 13:42:43 +0800

    Abstract header into seperate component

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import React from "react";
+import styles from "../styles/Dashboard.module.css";
+import Header from "../components/header";
 
 export default function Dashboard() {
   return (
     <div>
       <main>
-        <h1>Dashboard</h1>
+        <Header />
+        <h1 className={styles.t}>Dashboard</h1>
         <Link href="../">
           <a>Upload Receipt</a>
         </Link>
